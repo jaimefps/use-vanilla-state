c93ee9e81df9b12c0a7804a58184563128b1fd92
Jaime Pericas <jaime.pericas.saez@gmail.com>
2022-05-05 00:41:55 -0500

    adds type for useVanillaState input

--- src/module.ts
+++ src/module.ts
@@ -1,30 +1,31 @@
 import { useCallback, useState } from "react"
 
-// Extends the child class to include a rerender() method that
-// only needs to be referenced in the scope of React components,
-// outside of the implementation of state management.
-export class VanillaState {
-  protected rerender
+type Rerender = () => void
 
-  constructor(rerender: () => void) {
+// Provides a rerender() method to the subclasses that only
+// needs to be referenced in the scope of React components,
+// outside of the implementation of state management.
+export abstract class VanillaState {
+  rerender
+
+  constructor(rerender: Rerender) {
     this.rerender = rerender
   }
 }
 
-// TODO: type for "ClassDef"
-export function useVanillaState(ClassDef: any) {
-  // allows us to force a rerender in React.
+export function useVanillaState<T extends VanillaState>(CustomState: {
+  new (rerender: Rerender): T
+}) {
+  // allows us to force rerender
   const [, setNum] = useState(0)
 
-  // fed to `new ClassDef()` to allow us to extend the
-  // class instance to power the rerender() method.
   const rerender = useCallback(() => {
     setNum((num) => num + 1)
   }, [setNum])
 
-  // create instance within React:
+  // create stable ref for the state instance
   const [entity] = useState(() => {
-    return new ClassDef(rerender)
+    return new CustomState(rerender)
   })
 
   return entity
